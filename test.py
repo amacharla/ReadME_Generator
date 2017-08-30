e0b787a9bc5c5589efe1c629b0363b66ba1330a1
amacharla <noop2016@gmail.com>
2017-08-30 22:13:37 +0000

    headers done

--- test.py
+++ test.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup
 
 with open("everythingObject.htm") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
-    print(soup.pre)
-    file = open("workfile.md", "w")
-    file.write(str(soup.pre))
+    tasks = soup.find_all("h4", class_="task")
+    taskname = "".join([i.contents[0] for i in tasks])
+    print(taskname)
+    file = open("work.md", "w")
+    file.write(taskname)
     file.close()
 
--- workfile.md
+++ workfile.md
@@ -4,4 +4,10 @@
 &gt;&gt;&gt; b
 1
 &gt;&gt;&gt; 
+</code></pre><pre><code>&gt;&gt;&gt; a = 1
+&gt;&gt;&gt; b = a
+&gt;&gt;&gt; a = 2
+&gt;&gt;&gt; b
+1
+&gt;&gt;&gt; 
 </code></pre>

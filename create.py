1c26b05508a27e6a7146b421ccec3fcd5cf8831c
amacharla <noop2016@gmail.com>
2017-08-31 05:30:23 +0000

    added - to mandatory

--- create.py
+++ create.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 from bs4 import BeautifulSoup, Comment, NavigableString
-soup = BeautifulSoup(open("html"), 'html.parser')
 
-# Identify the start comment
-def begin_comment(text):
-    return (isinstance(text, Comment) and
-            text in "<!-- Task Body -->")
+# open html file and parse through source code
+soup = BeautifulSoup(open("htmlstuff.htm"), 'html.parser')
 
 # Identify the end location
 def end_mark(element):
     return element.string == "Repo:"
-i = 0
-readme = "BY NOOP LION"
-# Look for start comments
-for starting_point in soup.find_all(text=begin_comment): # get starting point
-    readme += ('==================={}=====================\n'.format(i))
-    i += 1
-    for element in starting_point.find_all_next(True): # get everything in between
-        if end_mark(element): # if end comment is found, quit
-            break
-        readme += str(element) + '\n'
 
-file = open("work.md", "w")
+readme = ""
+# Look for start comments
+for starting_point in soup.find_all(class_="task"): # set starting point
+    readme += str(starting_point) # add Task Header
+    for element in starting_point.find_next_siblings(True): # get all requirments and code
+        if end_mark(element): # stop before Repo info
+            readme += '\n\n'
+            break
+        readme += str(element)
+
+# Writing to ReadMe File
+file = open("work2.md", "w")
 file.write(readme)
 file.close()
--- test2.py
+++ test2.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
 from bs4 import BeautifulSoup, Comment, NavigableString
+
+# open html file and parse through source code
 soup = BeautifulSoup(open("htmlstuff.htm"), 'html.parser')
 
 # Identify the end location
 def end_mark(element):
     return element.string == "Repo:"
 
+def make_mine(header):
+    header.contents[1].insert(0, '-') # adds - mandatory
+    #print(header.find_next('p').contents.string) # Write -> Wrote
+    return(header)
 readme = ""
 # Look for start comments
-for starting_point in soup.find_all(class_="task"): # get starting point
-    readme += str(starting_point)
-    for element in starting_point.find_next_siblings(True):
-        if end_mark(element):
+for starting_point in soup.find_all(class_="task"): # set starting point
+    readme += str(make_mine(starting_point)) # add Task Header
+    for element in starting_point.find_next_siblings(True): # get all requirments and code
+        if end_mark(element): # stop before Repo info
             readme += '\n\n'
             break
         readme += str(element)
-file = open("work2.md", "w")
+#print(readme)
+
+# Writing to ReadMe File
+file = open("work3.md", "w")
 file.write(readme)
 file.close()
